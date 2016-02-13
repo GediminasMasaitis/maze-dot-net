6953e794b36f1bd768b9a43ed4c6a1c1e0024d7e
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2016-02-13 23:52:25 +0200

    Extracted a rectangle class from the recursive division generator to a non-nested class, added a dimension check and dimension property

---
+++ Maze.Core/Common/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using Maze.Core.Exceptions;
+
+namespace Maze.Core.Common
+{
+    public class Rectangle
+    {
+        public Rectangle(Point from, Point to)
+        {
+            if (from.Dimensions != to.Dimensions)
+            {
+                throw new IncorrectDimensionsException(new []{ from.Dimensions }, to.Dimensions);
+            }
+            From = from;
+            To = to;
+            Size = To - From;
+        }
+
+        public Point From { get; }
+        public Point To { get; }
+        public Point Size { get; }
+        public int Dimensions => From.Dimensions;
+
+        public bool Contains(Point point)
+        {
+            var contains = point[0] >= From[0] && point[1] >= From[1] && point[0] < To[0] && point[1] < To[1];
+            return contains;
+        }
+
+        public override string ToString()
+        {
+            return "From " + From + " to " + To + "; Size: " + Size;
+        }
+    }
+}
--- Maze.Core/Generators/RecursiveDivision/RecursiveDivisionMazeGenerator.cs
+++ Maze.Core/Generators/RecursiveDivision/RecursiveDivisionMazeGenerator.cs
@@ -105,32 +105,6 @@ namespace Maze.Core.Generators.RecursiveDivision
         }
 
         private int CurrentIteration { get; set; }
-
-        private class Rectangle
-        {
-            public Rectangle(Point from, Point to)
-            {
-                From = from;
-                To = to;
-                Size = To - From;
-            }
-
-            public Point From { get; }
-            public Point To { get; }
-            public Point Size { get; }
-
-            public bool Contains(Point point)
-            {
-                var contains = point[0] >= From[0] && point[1] >= From[1] && point[0] < To[0] && point[1] < To[1];
-                return contains;
-            }
-
-            public override string ToString()
-            {
-                return "From " + From + " to " + To + "; Size: " + Size;
-            }
-        }
-
         private IList<Rectangle> Rectangles { get; set; }
         private Rectangle CurrentRectangle { get; set; }
         private Point InitialPoint { get; set; }
